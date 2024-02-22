5f20227e4b3748d6658ed5098db4fb5bd21ebd14
Abhishek-esthala <100205320+Abhishek-esthala@users.noreply.github.com>
2024-02-22 14:05:10 +0530

    Create Home.jsx

---
+++ components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import BootstrapCarousel from './BootstrapCarousel'
+
+// import Header from './Header'
+
+export default function Home() {
+  return (
+    <div>
+    <BootstrapCarousel/>
+      <div>
+        <h4>Wecome!</h4>
+        <p>Welcome to this demo website. This is a smaple design developed using HTML, CSS, Javascript and React Framework.
+          Thanks for visiting our site. You can move to other components from the header section.
+        </p>
+      </div>
+    </div>
+  )
+}
